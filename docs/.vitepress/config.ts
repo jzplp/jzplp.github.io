23eed7b63c7316e99d334dba9b2f3cc3d982454f
jzplp <jzplp@qq.com>
2024-09-18 23:40:46 +0800

    文章大纲

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         text: '2024',
         items: [
           // { text: 'Webpack分包', link: '/2024/webpack-chunk' },
+          { text: 'JavaScript中的this, 究竟指向什么？', link: '/2024/js-this' },
           { text: '使用天地图与Leaflet，轻松创建免费地图应用', link: '/2024/tianditu-leaflet' },
           { text: 'CSS优先级，没你想的那么简单！全面介绍影响CSS优先级的各类因素', link: '/2024/css-specificity' },
           { text: 'Vue中的事件总线(EventBus)是什么？它有什么优点和缺点？', link: '/2024/vue-eventbus' },
